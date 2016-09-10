c858add8100ee9d651be007822fce9894c360cc9
Alexey <mechehuy@gmail.com>
2016-09-10 15:12:11 +0300

    add files

---
+++ js.js
@@ -0,0 +1,8 @@
+"use strict"
+//alert("Hello");
+
+function e(){
+	var name=document.getElementById("name").value;
+    var data=document.getElementById("data")
+	document.getElementById("aa").innerHTML=name;
+}
