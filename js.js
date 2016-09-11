8c095a3977d15b800fb8ddd5f20c4e5cf3553cd4
Alexey <mechehuy@gmail.com>
2016-09-11 10:21:25 +0300

    End

--- js.js
+++ js.js
@@ -1,8 +1,28 @@
 "use strict"
-//alert("Hello");
+
+
 
 function e(){
 	var name=document.getElementById("name").value;
-    var data=document.getElementById("data")
-	document.getElementById("aa").innerHTML=name;
+    var day=document.getElementById("day").value;
+	var month=document.getElementById("month").value;
+	var year=document.getElementById("year").value;
+	if (day==0 && month==0 && year==0 && day==0 && month==0 || name=='' || day>31 || month>12 || year>=2016){
+		alert("Неверние данные");
+	}else {
+	var age=2016-year;
+	month=12*age;
+	var week=age*52;
+	day=(365*age)+(age/4);
+	day=day-(day%1);
+	var hour=day*24;
+	var min=hour*60;
+	document.getElementById("t1").innerHTML=name;	
+	document.getElementById("t2").innerHTML=age;	
+	document.getElementById("t3").innerHTML=month;	
+	document.getElementById("t4").innerHTML=week;
+    document.getElementById("t5").innerHTML=day;	
+    document.getElementById("t6").innerHTML=hour;	
+	document.getElementById("t7").innerHTML=min;	
+	}
 }
